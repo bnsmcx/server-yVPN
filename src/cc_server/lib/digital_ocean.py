5114bad7c55b1040e457cd00a4140a489cbdaa85
ben <ben@bnsmcx.com>
2022-07-11 17:32:53 -0300

    Sketch out new droplet creation

--- src/cc_server/lib/digital_ocean.py
+++ src/cc_server/lib/digital_ocean.py
@@ -15,3 +15,21 @@ def get_available_datacenters() -> schemas.DataCenters:
         regions.append(region["slug"])
 
     return schemas.DataCenters(**{'available': regions})
+
+
+def create_droplet(settings: schemas.EndpointCreate) -> requests.models.Response:
+    image_id = 110391971 # TODO: this should not be hardcoded
+    request = {
+        "name": f"{endpoint_name}",
+        "region": f"{settings.region}",
+        "size": "s-1vcpu-1gb",
+        "image": image_id,
+        "ssh_keys": [
+            f"{ssh_fingerprint}"
+        ],
+    }
+
+    header = {"Authorization": f"Bearer {token}"}
+    response = requests.post(json=request,
+                             url="https://api.digitalocean.com/v2/droplets",
+                             headers=header)
