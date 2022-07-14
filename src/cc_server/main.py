01f1a795f93b8b3f664b5f3090f91695e22d544a
ben <ben@bnsmcx.com>
2022-07-14 16:47:00 -0300

    Run pylint against main.py

--- src/cc_server/main.py
+++ src/cc_server/main.py
@@ -1,3 +1,19 @@
+"""
+fastAPI app for the yourVPN rendezvous server.
+
+Functions -- API Endpoints:
+    create_user(user, database) -> User
+    read_users(skip, limit, database) -> List[User]
+    read_user(user_id, database) -> User
+    create_endpoint(settings, user_token, database) -> Endpoint
+    get_available_datacenters() -> List[str]
+    get_user_status(user_token, database) -> List[Endpoint]
+    delete_endpoint(user_token, endpoint_name, database) -> None
+
+Functions -- Internal Utility:
+    _get_database() -> Session
+"""
+
 from typing import List
 
 from fastapi import Depends, FastAPI, HTTPException
@@ -11,61 +27,67 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
-# Dependency
-def get_db():
-    db = SessionLocal()
+def _get_database():
+    """get and return a database session, ensure it closes"""
+    database = SessionLocal()
     try:
-        yield db
+        yield database
     finally:
-        db.close()
+        database.close()
 
 
 @app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user:
+def create_user(user: schemas.UserCreate, database: Session = Depends(_get_database)):
+    """create a new user"""
+    database_user = crud.get_user_by_email(database, email=user.email)
+    if database_user:
         raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
+    return crud.create_user(database, user)
 
 
 @app.get("/users/", response_model=list[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
+def read_users(skip: int = 0, limit: int = 100, database: Session = Depends(_get_database)):
+    """get and return a list of users"""
+    users = crud.get_users(database, skip=skip, limit=limit)
     return users
 
 
 @app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-    if db_user is None:
+def read_user(user_id: int, database: Session = Depends(_get_database)):
+    """return a specific user"""
+    database_user = crud.get_user(database, user_id=user_id)
+    if database_user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    return db_user
+    return database_user
 
 
 @app.post("/create", response_model=schemas.Endpoint)
 def create_endpoint(settings: schemas.EndpointCreate,
                     user_token: str,
-                    db: Session = Depends(get_db)):
-    if not crud.valid_user(db, user_token):
+                    database: Session = Depends(_get_database)):
+    """create a new endpoint"""
+    if not crud.valid_user(database, user_token):
         raise HTTPException(status_code=404, detail="Invalid user token")
-    else:
-        endpoint = crud.create_new_endpoint(db, settings, user_token)
-        return endpoint
+    endpoint = crud.create_new_endpoint(database, settings, user_token)
+    return endpoint
 
 
 @app.get("/datacenters", response_model=schemas.DataCenters)
 def get_available_datacenters():
+    """get a list of available digital ocean datacenters"""
     return digital_ocean.get_available_datacenters()
 
 
 @app.get("/status", response_model=List[schemas.Endpoint])
-def get_user_status(user_token: str, db: Session = Depends(get_db)):
-    user_endpoints = crud.get_user_endpoints(db, user_token)
+def get_user_status(user_token: str, database: Session = Depends(_get_database)):
+    """get a user's usage summary"""
+    user_endpoints = crud.get_user_endpoints(database, user_token)
     return user_endpoints
 
 
 @app.delete("/endpoint")
 def delete_endpoint(user_token: str,
                     endpoint_name: str,
-                    db: Session = Depends(get_db)):
-    crud.delete_endpoint(user_token, endpoint_name, db)
+                    database: Session = Depends(_get_database)):
+    """delete an endpoint"""
+    crud.delete_endpoint(user_token, endpoint_name, database)
